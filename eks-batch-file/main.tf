ebcc20900c292b6e37ec3083b3e01338111d5f56
Bhagyash-raut <bhagyashraut9@gmail.com>
2026-07-10 09:21:10 +0530

    Comment out variables in main.tf
    Commented out variable definitions and removed terraform.tfvars values.

--- eks-batch-file/main.tf
+++ eks-batch-file/main.tf
@@ -114,34 +114,20 @@ resource "aws_eks_node_group" "cbz_nodegroup" {
   ]
 }
 
-variable.tf file
-variable "project" {
-  type    = string
-  default = "lion"
-}
+#variable.tf file
+#variable "project" {
+#  type    = string
+#  default = "lion"
+#}
 
-variable "desired_nodes" {
-  type    = number
-  default = 2
-}
+#variable "desired_nodes" {
+#  type    = number
+#  default = 2
+#}
 
-variable "min_nodes" {
-  type    = number
-  default = 1
-}
-
-variable "max_nodes" {
-  type    = number
-  default = 3
-}
-
-variable "node_instance_type" {
-  type    = string
-  default = "c7i-flex.large"
-}
-
-
-terraform.tfvars
+#variable "min_nodes" {
+#  type    = number
+#
 
 project            = "cbz"
 desired_nodes      = 2
